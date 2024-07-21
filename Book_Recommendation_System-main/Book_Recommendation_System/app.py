43620ec596880f86f0a8c54eaacc493d88a900f9
ITRajavel <rajavelkumari22@gmail.com>
2024-07-21 17:12:29 +0530

    Book recommentation system using machine learning is done
    A Book Recommendation System using Machine Learning aims to suggest books to users based on their preferences and reading habits. This system typically employs techniques like collaborative filtering, which analyzes patterns in user behavior, and content-based filtering, which considers the attributes of books and user profiles. By integrating data such as user ratings, book genres, and historical interactions, the system predicts and recommends books that align with individual tastes, enhancing the user's reading experience. Advanced models may also incorporate natural language processing (NLP) to understand book descriptions and reviews, further refining the accuracy and relevance of recommendations.

---
+++ Book_Recommendation_System-main/Book_Recommendation_System/app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from func_file import *
+from string import punctuation
+from nltk.corpus import stopwords
+
+#####
+df = pd.read_csv('c:\\Users\\Admin\\Downloads\\Book_Recommendation_System-main\\Book_Recommendation_System-main\\prog_book.csv')
+
+
+
+#df = pd.read_csv(r'c:\Users\Admin\Downloads\Book_Recommendation_System-main\Book_Recommendation_System-main\prog_book.csv')
+import nltk
+nltk.download('stopwords')
+stop = stopwords.words('english')
+stop = set(stop)
+
+df['clean_Book_title']=df['Book_title'].apply(clean_text)
+df['clean_Description']=df['Description'].apply(clean_text)
+
+vectorizer = TfidfVectorizer(analyzer='word', lowercase=False)
+X = vectorizer.fit_transform(df['clean_Book_title'])
+title_vectors = X.toarray()
+
+tk = 0
+st.title('Programming Book Recommendation System')
+
+#col1, col2 = st.columns(2)
+
+col1, col2 = st.columns(2)
+
+#col1, col2 = st.beta_columns(2)
+#taking book name as input
+with col1:
+    book = st.text_input('Enter book name that you liked : ')
+    
+#taking multiple fiels to get similarity
+with col2:
+    feat = st.selectbox("Select Mode : ",['Book_title', 'Rating', 'Price'])
+    if st.button('Search'):
+        tk = 1
+
+#st.dataframe(df.head(10))
+if tk == 1:
+    st.success('Recommending books similar to '+book)
+    rec = st.empty()
+    rec = st.dataframe(get_recommendations(book, 'Book_title', df, title_vectors, feat), width=700, height=76)
+
+#print(get_recommendations())
